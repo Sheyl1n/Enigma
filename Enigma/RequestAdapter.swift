2566af6801531d1bc641764ff5e268383f5adc8f
Christian Bollig <christian.bollig@7p-group.com>
2020-11-14 00:24:09 +0100

    Requestadapter halfway

---
+++ Enigma/RequestAdapter.swift
@@ -0,0 +1,90 @@
+//
+//  RequestAdapter.swift
+//  Enigma
+//
+//  Created by Christian Bollig on 13.11.20.
+//  Copyright © 2020 SevenPrinciples. All rights reserved.
+//
+
+import UIKit
+
+enum NetworkError: Error {
+    case badURL
+    case badData
+}
+class RequestAdapter: NSObject, URLSessionDelegate {
+
+    static let current: RequestAdapter = RequestAdapter()
+
+    private var sharedSessionHolder: URLSession?
+    internal func sharedSession() -> URLSession {
+        guard sharedSessionHolder == nil else {
+            return sharedSessionHolder!
+        }
+
+        let sessionConfig = URLSessionConfiguration.default
+        sharedSessionHolder = URLSession(configuration: sessionConfig, delegate: self, delegateQueue: nil)
+
+        return sharedSessionHolder!
+    }
+
+    func getID(completionHandler: @escaping (Swift.Result<(HTTPURLResponse, Data), NetworkError>) -> Void) {
+
+        // Build up the URL
+        guard let url = URL(string: "http://5.45.106.224:8080/enigma/session") else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+
+        // Generate and execute the request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+
+        let task = sharedSession().dataTask(with: request) { (data, response, error) in
+            DispatchQueue.main.async {
+
+                guard let data = data, let response = response as? HTTPURLResponse else {
+                    completionHandler(.failure(.badURL))
+                    return
+                }
+                completionHandler(.success((response, data)))
+            }
+        }
+        task.resume()
+    }
+
+    func sendMSG(msg: String, sender: String, reciever: String, completionHandler: @escaping (Swift.Result<(HTTPURLResponse, Data), NetworkError>) -> Void) {
+
+        // Build up the URL
+        guard let url = URL(string: "http://5.45.106.224:8080/enigma/message") else {
+            completionHandler(.failure(.badURL))
+            return
+        }
+
+        // Generate and execute the request
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+
+        var dict: [String:Any] = ["receiverId":reciever]
+        let message: [String:Any] = ["senderId": sender, "text": msg]
+        dict["message"] = message
+        do {
+            let data = try JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted)
+            request.httpBody = data
+        } catch {
+            return completionHandler(.failure(.badData))
+        }
+
+        let task = sharedSession().dataTask(with: request) { (data, response, error) in
+            DispatchQueue.main.async {
+
+                guard let data = data, let response = response as? HTTPURLResponse else {
+                    completionHandler(.failure(.badURL))
+                    return
+                }
+                completionHandler(.success((response, data)))
+            }
+        }
+        task.resume()
+    }
+}
--- Enigma/ViewController.swift
+++ Enigma/ViewController.swift
@@ -10,16 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet var idLabel: UILabel?
+    @IBOutlet var textField: UITextField?
+    @IBOutlet var recieverField: UITextField?
+
+    @IBAction func sendAction() {
+        guard let msg = textField?.text, let reciever = recieverField?.text, let sender = id else {
+            return
+        }
 
         guard let config = Config(rotors: [.I,.II,.III], reflector:.UKW_B, setting: [0,0,0]) else {
             return
         }
         let engine = Enigma(withConfig: config)
-        print(engine.encrypt(message: "Hello World"))
+        let encryptedMSG = engine.encrypt(message: msg)
+
+        RequestAdapter.current.sendMSG(msg: encryptedMSG, sender: sender, reciever: reciever) { (result) in
+            switch result {
+
+            case .success((let response, let data)):
+                self.textField?.text = ""
+            case .failure(let error):
+                self.textField?.backgroundColor = UIColor.red
+            }
+        }
     }
 
+    @IBAction func recieveAction() {
+        
+    }
+
+    var id: String?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        getID()
+    }
+
+    func getID() {
+        RequestAdapter.current.getID { (result) in
+            switch result {
+
+            case .success((let response, let data)):
+                print(response)
+                guard let object = try? JSONSerialization.jsonObject(with: (data as Data?)!, options: []),
+                    let dict = object as? [String: Any] else {
+                        return
+                }
+
+                self.id = dict["id"] as? String
+                self.idLabel?.text = "ID: \(self.id ?? "Failed")"
+
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
 
 }
 
